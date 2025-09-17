4a442c5a696480071d7ca99d4a49aba3c54216bf
ragini <raginiambi1722@gmail.com>
2025-09-17 11:42:28 +0530

    Updated the question.py

--- Backend/questions.py
+++ Backend/questions.py
@@ -3,28 +3,26 @@ from db import get_connection
 def load_mmse_questions():
     questions = [
         # Orientation
-        ("What is the year?", "Orientation", 1),
-        ("What is the season?", "Orientation", 1),
-        ("What is the date today?", "Orientation", 1),
+        ("What is the year?", "Orientation", 2),
+        ("What is the season?", "Orientation", 2),
+        ("What is the date today?", "Orientation", 2),
         ("What day of the week is it?", "Orientation", 1),
-        ("What is the name of this place?", "Orientation", 1),
+        ("What is the name of this place?", "Orientation", 3),
         
         # Registration
-        ("I will name 3 objects. After I say them, repeat all three.", "Registration", 3),
+        ("From the given pictures choose the option with sequential order of the images", "Registration", 3),
 
         # Attention
         ("Subtract 7 from 100, and then keep subtracting 7 (5 times).", "Attention", 5),
 
         # Recall
-        ("Can you recall the 3 objects I named earlier?", "Recall", 3),
+        ("Can you recall the 3 objects I showed earlier?", "Recall", 3),
 
         # Language
-        ("Name the object I’m holding (e.g., pencil).", "Language", 1),
-        ("Repeat this phrase: 'No ifs, ands, or buts.'", "Language", 1),
-        ("Follow a 3-stage command: 'Take this paper in your right hand, fold it in half, and place it on the floor.'", "Language", 3),
-        ("Read and obey this: 'Close your eyes.'", "Language", 1),
+        ("Name the object I’m holding .", "Language", 1),
+        ("Write the phrase as it is: 'No ifs, ands, or buts.'", "Language", 1),
         ("Write a sentence of your choosing.", "Language", 1),
-        ("Copy this drawing of intersecting pentagons.", "Language", 1)
+      
     ]
 
     conn = get_connection()
